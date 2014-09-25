230e16982b8c3f72fd03b3579908831158fcd099
Devon Merrill <djmerrill@ucsd.edu>
2014-09-25 20:44:17 +0000

    Dummy schematic maker. No nets yets, but parts should be placeable
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/dummysch@18908 2cf53259-273a-af40-bda6-69d31023b8ec

--- MakeSchematics.py
+++ MakeSchematics.py
@@ -53,20 +53,36 @@ def make_eagle_device_schematics (gcom_dir, catalog, sch_template, libraries):
         home_dir = os.path.join(gcom_dir, home_dir)
         
         schematic_path = component.find("schematic")
-        
-        
+        keyname = component.get("keyname")
+            
         if schematic_path is None:
             # no schematic filename specified
             # so let's add one
             print "No schematic specified for", component.get("keyname")
             schematic_path = ET.Element("schematic")
-            schematic_path.set("filename", "eagle/device.auto.sch")
+            schematic_path.set("filename", "eagle/"+keyname+".device.auto.sch")
             component.append(schematic_path)
             
         schematic_path = os.path.join(home_dir, schematic_path.get("filename"))
         #print "Using:", schematic_path
         
-        if os.path.exists(schematic_path):
+        
+        placedparts = component.find("placedparts")
+        ref = None
+        if placedparts is not None:
+            placedparts = placedparts.findall("placedpart")
+        
+            if len(placedparts) > 1:
+                print "Could not create schematic for component: "+component.get("keyname")+". More than one placed part."
+            ref = placedparts[0].get("refdes")
+            
+        if ref is None:
+            ref = "U1"
+        
+        has_auto = False
+        if schematic_path.endswith(".auto.sch"): has_auto = True
+        
+        if os.path.exists(schematic_path) and not has_auto:
             #print schematic_path, "exists"
             pass
         else:
@@ -79,12 +95,15 @@ def make_eagle_device_schematics (gcom_dir, catalog, sch_template, libraries):
             eagledevice = component.find("eagledevice")
             
             if eagledevice is None:
-                print "Could not make auto schematic for:", component.get("keyname"), "no eagledevice specified."
+                print "Could not make auto schematic for:", component.get("keyname") + ". No eagledevice specified."
+                print
                 continue
             
             device_name = eagledevice.get("device-name")
             if device_name is None:
-                device_name = ""
+                device_name = eagledevice.get("name")
+            if device_name is None:
+                print "Could not create schematic for component:", component.get("keyname") + ". No device name in eagledevice entry."
             library_name = eagledevice.get("library")
             variant = eagledevice.get("variant")
             if variant is None: variant = ""
@@ -101,8 +120,6 @@ def make_eagle_device_schematics (gcom_dir, catalog, sch_template, libraries):
             
             instances = sch_template.find("./drawing/schematic/sheets/sheet/instances")
             
-            ref = "U1"
-            
             parts.append( ET.Element("part", name=ref, library=library_name, deviceset=device_name, device=variant, value=value) )
             
             
@@ -116,6 +133,7 @@ def make_eagle_device_schematics (gcom_dir, catalog, sch_template, libraries):
                 sch_template.find("./drawing/schematic/libraries").append(copy.deepcopy(library.find("./drawing/library")))
 
             sch_template.write(schematic_path)
+            print
             
     return catalog
             
