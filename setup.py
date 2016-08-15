2b5793c93a765251c32b80d9e2e2c66b523e5e91
Steven Swanson <swanson@cs.ucsd.edu>
2016-08-14 23:17:57 -0700

    Configure entry_points in setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,20 @@ with open(os.path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 with open(os.path.join(here, 'VERSION.txt'), encoding='utf-8') as f:
     version = f.read()
 
+
 setup(name="dummysch",
       version=version,
       long_description=long_description,
       author="NVSL, University of California San Diego",
-      packages = find_packages())
+      packages = find_packages(),
+      #install_requires=["jinja2", "Checkers"],
+      #dependency_links = ['ssh+git:git@github.com:NVSL/Checkers.git#egg=Checkers-0.1.0'],
+      #dependency_links = ['file://' + os.environ['GADGETRON_ROOT'] + "/Tools/Checkers#egg=Checkers-0.1.0",
+      #                ],
+      entry_points={
+          'console_scripts': [
+              "dummysch = dummy_sch.dummy_sch:main",
+              "dummysch.py = dummy_sch.dummy_sch:main"
+          ]
+      }
+)
